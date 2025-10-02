f66b510fafcd04361863dac8687ed8cdf622db8d
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-02 20:33:41 +0300

    add widget texture buffreing

--- inc/MyGUI.h
+++ inc/MyGUI.h
@@ -1,5 +1,5 @@
-#ifndef MGAPPLICATION_H
-#define MGAPPLICATION_H
+#ifndef MyGUI_H
+#define MyGUI_H
 
 #include <vector>
 
@@ -9,151 +9,29 @@
 #include "gm_primitives.hpp"
 #include "Events.h"
 
-class UIManager;
+
 class Widget;
-class Container;
-
-
-
-struct Rect : public SDL_Rect {
-    Rect(int x, int y, int w, int h) : SDL_Rect(x, y, w, h) {}
-    gm_dot<int, 2> pos() { return {x, y}; }
-};
-bool isInsideRect(const Rect& rect, const int x, const int y);
 
 
 class RendererGuard {
     SDL_Renderer* renderer_;
+    SDL_Texture* savedTarget_;
+    SDL_Rect savedViewport_;
+    SDL_Rect savedClip_;
     Uint8 r_, g_, b_, a_;
     SDL_BlendMode blend_;
-    SDL_Rect viewport_;
-    SDL_Rect clip_;
 
 public:
-    RendererGuard(SDL_Renderer* renderer) : renderer_(renderer) {
-        SDL_GetRenderDrawColor(renderer_, &r_, &g_, &b_, &a_);
-        SDL_GetRenderDrawBlendMode(renderer_, &blend_);
-        SDL_RenderGetViewport(renderer_, &viewport_);
-        SDL_RenderGetClipRect(renderer_, &clip_);
-    }
-
-    ~RendererGuard() {
-        SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
-        SDL_SetRenderDrawBlendMode(renderer_, blend_);
-        SDL_RenderSetViewport(renderer_, &viewport_);
-        SDL_RenderSetClipRect(renderer_, &clip_);
-    }
+    RendererGuard(SDL_Renderer* renderer);
+    ~RendererGuard();
 };
 
-class Widget {
-protected:
-    UIManager *UIManager_ = nullptr;
-    const Widget *parent_;
-
-    Rect rect_;
-
-    SDL_Texture* texture_ = nullptr;
-
-    bool needsReRender_ = false;    
-
-private:
-// Events
-    virtual bool onMouseDown(const MouseButtonEvent &event) = 0;
-    virtual bool onMouseUp(const MouseButtonEvent &event) = 0;
-// Main loop stages
-    virtual void update() = 0;
-    virtual void render() = 0;
-// User defined paintEvent
-    virtual void paintEvent() = 0;
-    
-public:
-    Widget(int x, int y, int width, int height, const Widget *parent=nullptr): 
-        parent_(parent), rect_({x, y, width, height}) {}
-    
-    void setParent(const Widget *parent) { parent_ = parent; } 
-    const Widget *parent() const { return parent_; }
-    const SDL_Texture* texture() const { return texture_; }
-    Rect rect() const { return rect_; }
-
-friend class UIManager;
-friend class Container;
+struct Rect : public SDL_Rect {
+    Rect(int x = 0, int y = 0, int w = 0, int h = 0);
+    gm_dot<int, 2> pos() const;
 };
 
-class Container : public Widget {
-    std::vector<Widget *> children_;
-
-protected:
-// Events
-    bool onMouseDown(const MouseButtonEvent &event) override {
-        if (!isInsideRect(rect_, event.pos.x, event.pos.y)) return false;
-
-        for (Widget *child : children_) {
-            MouseButtonEvent childLocal = event;
-            childLocal.pos -= child->rect().pos();
-            if (!child->onMouseDown(childLocal)) {
-                return false; // stop event propagation
-            }
-        }
-
-        return true; // continue event propagation
-    };
-
-    bool onMouseUp(const MouseButtonEvent &event) override {
-        if (!isInsideRect(rect_, event.pos.x, event.pos.y)) return false;
-
-        for (Widget *child : children_) {
-            MouseButtonEvent childLocal = event;
-            childLocal.pos -= child->rect().pos();
-            if (!child->onMouseUp(childLocal)) {
-                return false; // stop event propagation
-            }
-        }
-
-        return true; // continue event propagation
-    };
-
-// Main loop stages
-    void update() override {
-        return;
-    };
-
-    void render() override {
-        RendererGuard rendererGuard();
-
-        SDL_Texture* texture_ = SDL_CreateTexture(UIManager_->renderer(),
-                                                  SDL_PIXELFORMAT_RGBA8888,
-                                                  SDL_TEXTUREACCESS_TARGET,
-                                                  rect_.w, rect_.h);
-        
-        SDL_SetRenderTarget(UIManager_->renderer(), texture_);
-        
-        SDL_Rect clip = { 0, 0, rect_.w, rect_.h };
-        SDL_RenderSetClipRect(UIManager_->renderer(), &clip);
-                
-        for (Widget *child : children_) {
-            child->render();
-            SDL_Rect chldRect = child->rect();
-            SDL_RenderCopy(UIManager_->renderer(), child->texture_, NULL, &chldRect);
-        }
-    };
-
-public:
-    Container(int x, int y, int width, int height, const Widget *parent=nullptr): Widget(x, y, width, height, parent) {}
-    ~Container() {
-        for (Widget *child : children_)
-            delete child;
-    }
-
-    void addWdiget(Widget *widget) {
-        if (widget->parent() == this || widget->parent() == nullptr) {
-            widget->setParent(this);
-            children_.push_back(widget); 
-        } else {
-            std::cerr << "addWdiget failed : parent does not match\n";
-        }
-    }
-};
-
+bool isInsideRect(const Rect& rect, const int x, const int y);
 
 struct UIManagerglobalState {
     Widget *target = nullptr;
@@ -162,76 +40,75 @@ struct UIManagerglobalState {
 class UIManager {
     Widget *wTreeRoot_ = nullptr;
     UIManagerglobalState glState_;
+    Uint32 frameDelay_;
 
-    SDL_Renderer* renderer_ = nullptr;
-
-    Uint32 frameDelay_ = 32;
-
-    
+    SDL_Renderer *renderer_ = nullptr;
+    SDL_Window *mainWindow_ = nullptr;
 
 private:
-    void resetTarget() { glState_.target = nullptr; }
-    void setTarget(Widget *newTarget) { glState_.target = newTarget; }
+    void handleSDLEvents(bool *running);
 
-    void handleSDLEvents(bool *running) {
-        SDL_Event SDLEvent;
+public: // user API
+    UIManager(int width, int height, Uint32 frameDelay = 32);
+    ~UIManager();
 
-         while (SDL_PollEvent(&SDLEvent)) {
-            if (SDLEvent.type == SDL_QUIT || SDLEvent.type == SDL_KEYDOWN && SDLEvent.key.keysym.sym == SDLK_ESCAPE) {
-                *running = false;
-                break;
-            }
-            
-            switch (SDLEvent.type) {
-            case SDL_MOUSEBUTTONDOWN:
-                wTreeRoot_->onMouseDown({SDLEvent.button.x, SDLEvent.button.y, SDLEvent.button.button});
-                break;
-            case SDL_MOUSEBUTTONUP:
-                wTreeRoot_->onMouseUp({SDLEvent.button.x, SDLEvent.button.y, SDLEvent.button.button});
-                break;
-            default:
-                break;
-            }
-        }
-    }
-
-public:
-    explicit UIManager(Uint32 frameDelay): frameDelay_(frameDelay) {}
-
-    void setMainWidget(Widget *mainWidget) {
-        if (wTreeRoot_ != nullptr) {
-            std::cerr << "setMainWidget failed : wTreeRoot != nullptr\n";
-            return;
-        }
-    
-        wTreeRoot_ = mainWidget;
-    }
-
-
-    SDL_Renderer* renderer() { return renderer_; }
-    
-    void run() {
-        bool running = true;
-        while (running) {
-            Uint32 frameStart =  SDL_GetTicks();
-
-            handleSDLEvents(&running);
-            wTreeRoot_->update();
-            
-            // for (auto userEvent : userEvents_) {
-            //     userEvent(frameDelay);
-            // }
-            
-            wTreeRoot_->render();
-
-            Uint32 frameTime = SDL_GetTicks() - frameStart;
-            if (frameDelay_ > frameTime) SDL_Delay(frameDelay_ - frameTime);
-        }
-    }
+    void setMainWidget(Widget *mainWidget);
+    void run();
 
 friend class Widget;
 };
 
+class Widget {
+protected:
+    UIManager *UIManager_ = nullptr;
+    const Widget *parent_ = nullptr;
+
+    Rect rect_;
+    SDL_Texture* texture_ = nullptr;
+
+    bool needsReRender_ = true;
+
+public:
+    Widget(int x, int y, int width, int height, const Widget *parent = nullptr);
+    virtual ~Widget();
 
 
-#endif // MGAPPLICATION_H
+    virtual void paintEvent(SDL_Renderer* renderer);
+    virtual void render(SDL_Renderer* renderer);
+    virtual void update();
+
+    // Events: return false to stop propagation (CONSUME), true to continue (PROPAGATE)
+    virtual bool onMouseDown(const MouseButtonEvent &event);
+    virtual bool onMouseUp(const MouseButtonEvent &event);
+
+    // Getters / Setters
+    bool needsReRender() const { return needsReRender_; }
+    Rect rect() const;
+    const Widget *parent() const;
+    void setParent(const Widget *parent);
+    SDL_Texture* texture();
+
+friend class UIManager;
+};
+
+class Container : public Widget {
+    std::vector<Widget *> children_;
+
+public:
+    Container(int x, int y, int width, int height, const Widget *parent = nullptr);
+    ~Container();
+
+    // Events
+    bool onMouseDown(const MouseButtonEvent &event) override;
+    bool onMouseUp(const MouseButtonEvent &event) override;
+
+    // Stages
+    void update() override;
+    void render(SDL_Renderer* renderer) override;
+
+    // User API
+    void addWdiget(Widget *widget);
+};
+
+
+#endif // MyGUI_H
--- src/MyGUI.cpp
+++ src/MyGUI.cpp
@@ -1,8 +1,304 @@
 #include "MyGUI.h"
 
-bool isInsideRect(const Rect& rect, const int x, const int y) {
-    return x >= rect.x && x <= (rect.x + rect.w) && y >= rect.y && y <= rect.y + rect.h;
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+// ---------------- RendererGuard ----------------
+
+inline bool isNullRect(const SDL_Rect &rect) {
+    return (rect.x == 0 && rect.y == 0 &&
+            rect.w == 0 && rect.h == 0);
 }
+
+RendererGuard::RendererGuard(SDL_Renderer* renderer) : renderer_(renderer) {
+    savedTarget_ = SDL_GetRenderTarget(renderer_);
+    SDL_GetRenderDrawColor(renderer_, &r_, &g_, &b_, &a_);
+    SDL_GetRenderDrawBlendMode(renderer_, &blend_);
+    SDL_RenderGetViewport(renderer_, &savedViewport_);
+    SDL_RenderGetClipRect(renderer_, &savedClip_);
+}
+
+RendererGuard::~RendererGuard() {
+    SDL_SetRenderTarget(renderer_, savedTarget_);      
+    SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_); 
+    SDL_SetRenderDrawBlendMode(renderer_, blend_);     
+
+    if (!isNullRect(savedViewport_))
+        SDL_RenderSetViewport(renderer_, &savedViewport_);
+    else
+        SDL_RenderSetViewport(renderer_, nullptr);
+
+    if (!isNullRect(savedClip_))
+        SDL_RenderSetClipRect(renderer_, &savedClip_);
+    else
+        SDL_RenderSetClipRect(renderer_, nullptr);
+}
+
+
+// ---------------- Rect / util ----------------
+
+Rect::Rect(int x, int y, int w, int h) {
+    this->x = x;
+    this->y = y;
+    this->w = w;
+    this->h = h;
+}
+
+gm_dot<int, 2> Rect::pos() const {
+    return { x, y };
+}
+
+bool isInsideRect(const Rect& rect, const int x, const int y) {
+    return (x >= rect.x) && (x < rect.x + rect.w) && (y >= rect.y) && (y < rect.y + rect.h);
+}
+
+// ---------------- UIManager ----------------
+
+UIManager::UIManager(int width, int height, Uint32 frameDelay)
+    : frameDelay_(frameDelay)
+{
+    mainWindow_ = SDL_CreateWindow(
+        nullptr,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        width,
+        height,
+        0);
+
+    if (!mainWindow_) throw std::runtime_error(std::string("SDL_CreateWindow failed: ") + SDL_GetError());
+
+    renderer_ = SDL_CreateRenderer(mainWindow_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer_) {
+        SDL_DestroyWindow(mainWindow_);
+        mainWindow_ = nullptr;
+        throw std::runtime_error(std::string("SDL_CreateRenderer failed: ") + SDL_GetError());
+    }
+}
+
+UIManager::~UIManager() {
+    if (wTreeRoot_) delete wTreeRoot_;
+    if (mainWindow_) SDL_DestroyWindow(mainWindow_);
+    if (renderer_) SDL_DestroyRenderer(renderer_);
+    SDL_Quit();
+}
+
+void UIManager::setMainWidget(Widget *mainWidget) {
+    if (wTreeRoot_ != nullptr) {
+        std::cerr << "setMainWidget failed : wTreeRoot != nullptr\n";
+        return;
+    }
+
+    wTreeRoot_ = mainWidget;
+}
+
+void UIManager::handleSDLEvents(bool *running) {
+    SDL_Event SDLEvent;
+
+        while (SDL_PollEvent(&SDLEvent)) {
+        if (SDLEvent.type == SDL_QUIT || SDLEvent.type == SDL_KEYDOWN && SDLEvent.key.keysym.sym == SDLK_ESCAPE) {
+            *running = false;
+            break;
+        }
+        
+        switch (SDLEvent.type) {
+        case SDL_MOUSEBUTTONDOWN:
+            wTreeRoot_->onMouseDown({SDLEvent.button.x, SDLEvent.button.y, SDLEvent.button.button});
+            break;
+        case SDL_MOUSEBUTTONUP:
+            wTreeRoot_->onMouseUp({SDLEvent.button.x, SDLEvent.button.y, SDLEvent.button.button});
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void UIManager::run() {
+    if (!renderer_ || !mainWindow_) {
+        throw std::runtime_error("UIManager::run: window/renderer not initialized");
+    }
+
+    bool running = true;
+    while (running) {
+        Uint32 frameStart = SDL_GetTicks();
+
+        handleSDLEvents(&running);
+
+        // update tree
+        if (wTreeRoot_) wTreeRoot_->update();
+
+        // render pass
+        SDL_SetRenderDrawColor(renderer_, 50, 50, 50, 255);
+        SDL_RenderClear(renderer_);
+
+        if (wTreeRoot_) {
+            wTreeRoot_->render(renderer_);
+            assert(wTreeRoot_->texture());
+
+            SDL_Rect dst = wTreeRoot_->rect();
+            SDL_RenderCopy(renderer_, wTreeRoot_->texture(), NULL, &dst);
+        }
+
+        SDL_RenderPresent(renderer_);
+
+        // frame pacing
+        Uint32 frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay_ > frameTime) SDL_Delay(frameDelay_ - frameTime);
+    }
+}
+
+// ---------------- Widget ----------------
+
+Widget::Widget(int x, int y, int width, int height, const Widget *parent)
+    : parent_(parent), rect_(x, y, width, height)
+{}
+
+Widget::~Widget() {
+    if (texture_) {
+        SDL_DestroyTexture(texture_);
+        texture_ = nullptr;
+    }
+}
+
+void Widget::paintEvent(SDL_Renderer* renderer) {
+    assert(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white
+    SDL_Rect full = {0, 0, rect_.w, rect_.h};
+    SDL_RenderFillRect(renderer, &full);
+}
+
+void Widget::render(SDL_Renderer* renderer) {
+    if (!needsReRender_) return;
+
+    RendererGuard RendererGuard(renderer);
+
+    if (texture_) SDL_DestroyTexture(texture_);        
+
+    texture_ = SDL_CreateTexture(renderer,
+                                 SDL_PIXELFORMAT_RGBA8888,
+                                 SDL_TEXTUREACCESS_TARGET,
+                                 rect_.w, rect_.h);
+
+
+    SDL_SetRenderTarget(renderer, texture_);
+    SDL_RenderClear(renderer);
+
+    paintEvent(renderer);
+
+    needsReRender_ = false;
+}
+
+void Widget::update() {
+    // default: do nothing
+}
+
+bool Widget::onMouseDown(const MouseButtonEvent & /*event*/) {
+    return true;
+}
+bool Widget::onMouseUp(const MouseButtonEvent & /*event*/) {
+    return true;
+}
+
+Rect Widget::rect() const { return rect_; }
+const Widget *Widget::parent() const { return parent_; }
+void Widget::setParent(const Widget *parent) { parent_ = parent; }
+SDL_Texture* Widget::texture() { return texture_; }
+
+// ---------------- Container ----------------
+
+Container::Container(int x, int y, int width, int height, const Widget *parent)
+    : Widget(x, y, width, height, parent) {}
+
+Container::~Container() {
+    for (Widget *child : children_) delete child;
+    children_.clear();
+}
+
+bool Container::onMouseDown(const MouseButtonEvent &event) {
+    if (!isInsideRect(rect_, event.pos.x, event.pos.y)) return true; 
+
+    for (Widget *child : children_) {
+        MouseButtonEvent childLocal = event;
+
+        childLocal.pos.x -= child->rect().x;
+        childLocal.pos.y -= child->rect().y;
+        if (!child->onMouseDown(childLocal)) {
+            return false; // stop propagation
+        }
+    }
+
+    return true; // propagate
+}
+
+bool Container::onMouseUp(const MouseButtonEvent &event) {
+    if (!isInsideRect(rect_, event.pos.x, event.pos.y)) return true;
+
+    for (Widget *child : children_) {
+        MouseButtonEvent childLocal = event;
+        childLocal.pos.x -= child->rect().x;
+        childLocal.pos.y -= child->rect().y;
+        if (!child->onMouseUp(childLocal)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Container::update() {
+    for (Widget *child : children_) child->update();
+}
+
+void Container::render(SDL_Renderer* renderer) {
+    assert(renderer);
+
+    bool needsReRender = needsReRender_;
+
+    for (Widget *child : children_) {
+        needsReRender |= child->needsReRender();
+    }
+    if (!needsReRender) return;
+
+    RendererGuard rendererGuard(renderer);
+
+    if (texture_) SDL_DestroyTexture(texture_);     
+
+    texture_ = SDL_CreateTexture(renderer,
+                                 SDL_PIXELFORMAT_RGBA8888,
+                                 SDL_TEXTUREACCESS_TARGET,
+                                 rect_.w, rect_.h);
+
+    SDL_SetRenderTarget(renderer, texture_);
+    SDL_RenderClear(renderer);
+    paintEvent(renderer);
+    
+    SDL_Rect clip = { 0, 0, rect_.w, rect_.h };
+    SDL_RenderSetClipRect(renderer, &clip);
+    // render children into the texture; children should produce their own texture
+    for (Widget *child : children_) {
+        child->render(renderer);
+        if (child->texture()) {
+            SDL_Rect chldRect = child->rect();
+            SDL_RenderCopy(renderer, child->texture(), NULL, &chldRect);
+        }
+    }
+
+    needsReRender_ = false;   
+}
+
+void Container::addWdiget(Widget *widget) {
+    if (widget->parent() == this || widget->parent() == nullptr) {
+        widget->setParent(this);
+        children_.push_back(widget);
+    } else {
+        std::cerr << "addWdiget failed : parent does not match\n";
+    }
+}
+
+
 
 // bool isMouseEvent(const SDL_Event &event) {
 //     return  event.type == SDL_MOUSEMOTION ||
@@ -28,3 +324,4 @@ bool isInsideRect(const Rect& rect, const int x, const int y) {
 
 //     return resultTexture; 
 // }
+
